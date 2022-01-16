1288847f5fb4644c38a7b3541f5716e927ba2f1f
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-16 09:13:28 -0800

    modified BlockSerializer()

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,12 +1,32 @@
 from rest_framework import serializers
 from .models import Block, Room
+from .utils import create_room_block, create_room
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class BlockSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Block
-        fields = '__all_'
+        fields = '__all__'
+
+    def create(self, validated_data):
+        try:
+            create_room_block(
+                validated_data['name'],
+                validated_data['total_rooms']
+            )
+
+        except Exception as err:
+            logger.error('BlockSerializer.create@Error')
+            logger.error(err)
+            return None, str(err)
+
+    def update(self, instance, validated_data):
+        pass
 
 
 class RoomSerializer(serializers.ModelSerializer):
@@ -15,3 +35,9 @@ class RoomSerializer(serializers.ModelSerializer):
         model = Room
         fields = '__all__'
         read_only_fields = ['code']
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
