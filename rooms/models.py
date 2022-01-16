c2c986f3c72d486bb6e0939f49f561dca30fbe63
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-16 08:21:31 -0800

    created Block and Room models

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Block(models.Model):
+    name = models.CharField(max_length=5)
+    total_rooms = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    room_block = models.ForeignKey(Block, related_name='rooms', on_delete=models.CASCADE)
+    room_no = models.CharField(max_length=10)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.room_block.name} {self.room_no}'
