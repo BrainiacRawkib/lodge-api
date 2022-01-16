76b113da75347ee121ff3806cb5f8aa66c3f025e
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-16 01:10:13 -0800

    added rooms & users apps, and also rest_framework

--- lodging/settings.py
+++ lodging/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # apps
+    'rooms',
+    'users',
+
+    # third-party
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
