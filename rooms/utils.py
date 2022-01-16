970ed2d939d84011de3267e155a5f8ca4627da93
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-16 08:32:49 -0800

    created utils.py

---
+++ rooms/utils.py
@@ -0,0 +1,31 @@
+import string
+import random
+from .models import Block, Room
+
+
+# generate
+def generate_room_code():
+    length = 10
+    while True:
+        code = ''.join(random.choices(string.ascii_letters, k=length))
+        if Room.objects.filter(code=code).exists():
+            # if True, generate another code
+            generate_room_code()
+        else:
+            # if False, stop.
+            break
+    return code
+
+
+# create
+def create_block():
+    pass
+
+
+def create_room():
+    pass
+
+
+# get
+def get_room(room_):
+    pass
