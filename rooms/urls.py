a2e7387fba0d14d58ce7fe4525b4009fff96e669
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-17 09:39:29 -0800

    created url path for BlockAPI

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Block urls
+    path('blocks/', views.BlockAPI.as_view()),
+    path('create-block/', views.BlockAPI.as_view()),
+
+    # Room urls
     path('rooms/', views.RoomAPI.as_view()),
     path('create-room/', views.RoomAPI.as_view()),
 ]
