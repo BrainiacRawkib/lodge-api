b06b0e1a12839d4bc7f19ee5be50998001fa2ed2
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-17 10:11:56 -0800

    created url path for BlockAPI.delete()

--- rooms/urls.py
+++ rooms/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Block urls
     path('blocks/', views.BlockAPI.as_view()),
     path('create-block/', views.BlockAPI.as_view()),
+    path('delete-block/', views.BlockAPI.as_view()),
 
     # Room urls
     path('rooms/', views.RoomAPI.as_view()),
