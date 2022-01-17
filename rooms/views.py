79ff0a30a87707cc1ec8dec8d60dd978ed6c3637
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-16 22:41:29 -0800

    created RoomAPI() view

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .utils import *
 
-# Create your views here.
+
+class RoomAPI(APIView):
+
+    def get(self, request, id=None, format=None):
+        pass
+
+    def post(self, request, format=None):
+        pass
+
+    def put(self, request, id=None, format=None):
+        pass
+
+    def delete(self, request, id=None, format=None):
+        pass
