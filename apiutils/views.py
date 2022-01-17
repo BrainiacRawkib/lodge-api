fe9b637a8d8828a180f309cda992b491b0465db4
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-17 07:45:19 -0800

    created http_response()

--- apiutils/views.py
+++ apiutils/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
 
-# Create your views here.
+
+def http_response(msg, status, data=None):
+    if data is None:
+        data = {}
+
+    response_data = dict(
+        msg=msg, data=data
+    )
+
+    return Response(response_data, status=status)
