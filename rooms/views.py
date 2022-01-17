fc251737dbca98c4adfd4e9895ee897655a48ea8
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-16 22:44:56 -0800

    created BlockAPI() view

--- rooms/views.py
+++ rooms/views.py
@@ -2,7 +2,23 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from .serializers import BlockSerializer, RoomSerializer
 from .utils import *
+
+
+class BlockAPI(APIView):
+
+    def get(self, request, id=None, format=None):
+        pass
+
+    def post(self, request, format=None):
+        pass
+
+    def put(self, request, id=None, format=None):
+        pass
+
+    def delete(self, request, id=None, format=None):
+        pass
 
 
 class RoomAPI(APIView):
