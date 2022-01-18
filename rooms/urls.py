299bc163cdb762c39782f28f4183ba30a494e73d
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-18 08:23:45 -0800

    modified urls.py

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     # Block urls
     path('blocks/', views.BlockAPI.as_view()),
-    path('delete-block/<int:id>/', views.BlockAPI.as_view()),
+    path('blocks/<int:id>/', views.BlockAPI.as_view()),
 
     # Room urls
     path('rooms/', views.RoomAPI.as_view()),
-    path('delete-room/<str:code>/', views.RoomAPI.as_view()),
+    path('rooms/<str:code>/', views.RoomAPI.as_view()),
 ]
