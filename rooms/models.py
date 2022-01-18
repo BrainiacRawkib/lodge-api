ecbe477e7e426bf556fbcb53c6319d5ccdd8dff4
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-18 00:10:05 -0800

    modified code field in Room model

--- rooms/models.py
+++ rooms/models.py
@@ -10,7 +10,7 @@ class Block(models.Model):
 
 
 class Room(models.Model):
-    code = models.CharField(max_length=10, unique=True)
+    code = models.CharField(max_length=10, unique=True, default='', null=True)
     room_block = models.ForeignKey(Block, related_name='rooms', on_delete=models.CASCADE)
     room_no = models.CharField(max_length=10)
     available = models.BooleanField(default=True)
