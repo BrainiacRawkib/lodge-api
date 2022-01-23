6ed853b2a9cb7223bbceaeb88241d50898349736
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-23 04:24:57 -0800

    added Profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField('auth.User', related_name='users', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
