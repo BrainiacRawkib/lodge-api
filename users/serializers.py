d2cdf0a2aa33721fb406a0659c2893abfa1d8a1e
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-23 04:37:34 -0800

    removed ProfileSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Profile
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -8,10 +7,3 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email']
-
-
-class ProfileSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Profile
-        fields = '__all__'
