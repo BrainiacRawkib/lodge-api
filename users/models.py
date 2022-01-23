59641ebc0314a320c470543a5ae136874225f8ff
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-23 04:37:06 -0800

    removed Profile model

--- users/models.py
+++ users/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
-
-class Profile(models.Model):
-    user = models.OneToOneField('auth.User', related_name='users', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.user.username
+# Create your models here.
