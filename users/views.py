fac8ada5f384a8ff7aba7191b057035c811b9b67
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-24 02:42:16 -0800

    modified UserAPI.get()

--- users/views.py
+++ users/views.py
@@ -1,9 +1,18 @@
+from apiutils.views import http_response
 from rest_framework.views import APIView
+from rest_framework import status
+from .serializers import UserSerializer
 
 
 class UserAPI(APIView):
     def get(self, request, *args, **kwargs):
-        pass
+        user = request.user
+        serializer = UserSerializer(data=user)
+        return http_response(
+            'User Retrieved',
+            status=status.HTTP_200_OK,
+            data=serializer.data
+        )
 
     def post(self, request, *args, **kwargs):
         pass
