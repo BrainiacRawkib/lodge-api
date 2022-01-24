41679368e0edfb34600d726a34d2d0c1af706497
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-24 02:53:48 -0800

    modified main urls

--- lodging/urls.py
+++ lodging/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('rooms.urls')),
-    path('users/', include('users.urls')),
+    path('', include('rooms.urls')),
+    path('', include('users.urls')),
 ]
