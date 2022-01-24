f7def0b90a2b0e3df70a5024ad821aeae4b9c2f6
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-24 02:50:55 -0800

    modified UserAPI.get()

--- users/views.py
+++ users/views.py
@@ -8,9 +8,15 @@ class UserAPI(APIView):
     def get(self, request, *args, **kwargs):
         user = request.user
         serializer = UserSerializer(data=user)
+        if serializer.is_valid():
+            return http_response(
+                'User Retrieved',
+                status=status.HTTP_200_OK,
+                data=serializer.data
+            )
         return http_response(
-            'User Retrieved',
-            status=status.HTTP_200_OK,
+            'Error Retrieving User',
+            status=status.HTTP_400_BAD_REQUEST,
             data=serializer.data
         )
 
