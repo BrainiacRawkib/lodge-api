ec9b041786c3cab8db30f524dd63f071f3ab1a7f
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-31 03:31:22 -0800

    added mechanism to get users token

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from rest_framework.authtoken import views as rest_views
 from . import views
 
 urlpatterns = [
     path('api/users/', views.UserAPI.as_view()),
+    path('api/get-auth-token/', rest_views.obtain_auth_token),
 ]
