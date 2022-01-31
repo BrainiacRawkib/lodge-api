e99181a41136ee4adc013e35e8ce3fc54c12efd2
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-31 11:37:29 -0800

    added url path for LoginAPI()

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('api/users/', views.UserAPI.as_view()),
+    path('api/users/auth/', views.LoginAPI.as_view()),
     path('api/get-auth-token/', rest_views.obtain_auth_token),
 ]
