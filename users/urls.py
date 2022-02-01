368a32a20e2304e7d9ced76b33ab2adab065f525
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-31 18:39:34 -0800

    added url path for CustomAuthToken()

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from rest_framework.authtoken import views as rest_views
 from . import views
 
 urlpatterns = [
     path('api/users/', views.UserAPI.as_view()),
     path('api/users/auth/', views.LoginAPI.as_view()),
-    path('api/get-auth-token/', rest_views.obtain_auth_token),
+    path('api/get-auth-token/', views.CustomAuthToken.as_view()),
 ]
