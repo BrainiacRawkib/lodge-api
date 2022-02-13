51073f96eb7cc254e615358b3ad83a5039f29e23
Brainiac Rawkib <brainiacrawkib@gmail.com>
2022-02-12 18:43:40 -0800

    updated rooms codebase

--- rooms/views.py
+++ rooms/views.py
@@ -8,7 +8,14 @@ from .utils import *
 class BlockAPI(APIView):
     lookup_url_kwarg = 'id'
 
-    def get(self, request, id=None, format=None):
+    def get(self, request, format=None):
+        token = request.headers.get('Authorization')
+        if not token:
+            return http_response(
+                'Bad Request. Access Token missing.',
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
         room_blocks = get_all_room_blocks()
         serializer = BlockSerializer(room_blocks, many=True)
         query_params = self.request.query_params
@@ -29,6 +36,13 @@ class BlockAPI(APIView):
         )
 
     def post(self, request, format=None):
+        token = request.headers.get('Authorization')
+        if not token:
+            return http_response(
+                'Bad Request. Access Token missing.',
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
         payload = request.data
         serializer = BlockSerializer(data=payload)
         if serializer.is_valid():
@@ -46,10 +60,17 @@ class BlockAPI(APIView):
             data=payload
         )
 
-    def put(self, request, id=None, format=None):
+    def put(self, request, format=None):
         pass
 
-    def delete(self, request, id=None, format=None):
+    def delete(self, request, format=None):
+        token = request.headers.get('Authorization')
+        if not token:
+            return http_response(
+                'Bad Request. Access Token missing.',
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
         query_params = self.request.query_params
         if query_params:
             block_id = query_params.get(self.lookup_url_kwarg)
@@ -76,6 +97,13 @@ class RoomAPI(APIView):
     lookup_url_kwarg = 'code'
 
     def get(self, request, format=None):
+        token = request.headers.get('Authorization')
+        if not token:
+            return http_response(
+                'Bad Request. Access Token missing.',
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
         rooms = get_all_rooms()
         serializer = RoomSerializer(rooms, many=True)
         query_params = self.request.query_params
@@ -96,8 +124,16 @@ class RoomAPI(APIView):
         )
 
     def post(self, request, format=None):
+        token = request.headers.get('Authorization')
+        if not token:
+            return http_response(
+                'Bad Request. Access Token missing.',
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
         payload = request.data
         serializer = RoomSerializer(data=payload)
+
         if serializer.is_valid():
             data = serializer.validated_data
             data['room_block'] = get_room_block(payload['room_block'])
@@ -120,24 +156,36 @@ class RoomAPI(APIView):
         )
 
     def put(self, request, format=None):
+        token = request.headers.get('Authorization')
+        if not token:
+            return http_response(
+                'Bad Request. Access Token missing.',
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        user = get_user_by_token(token)
         query_params = request.query_params
         payload = request.data
-        user = get_user(request.user)
+
         if query_params:
             room_code = query_params.get(self.lookup_url_kwarg)
             room = get_room(room_code)
             payload['room_no'] = room.room_no
             payload['total_occupants'] = room.total_occupants
+
             if room:
                 serializer = RoomSerializer(data=payload)
+
                 if serializer.is_valid():
                     data = serializer.validated_data
+
                     if not room.available:
                         return http_response(
                             'Cannot add user to room. Room is full',
                             status=status.HTTP_204_NO_CONTENT,
                             data=serializer.data
                         )
+
                     room_to_update, _ = serializer.update(room, data, user)
                     if room_to_update:
                         return http_response(
@@ -145,6 +193,7 @@ class RoomAPI(APIView):
                             status=status.HTTP_200_OK,
                             data=serializer.data
                         )
+
                     return http_response(
                         'Error adding user to room.',
                         status=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -162,7 +211,14 @@ class RoomAPI(APIView):
         )
 
     def delete(self, request, format=None):
-        query_params = self.request.query_params
+        token = request.headers.get('Authorization')
+        if not token:
+            return http_response(
+                'Bad Request. Access Token missing.',
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        query_params = request.query_params
         if query_params:
             code = query_params.get(self.lookup_url_kwarg)
             if code:
